c4982e0ed0c323bd95ec4fca7f5ae44ad50d5ae1
florianwoelki <florianwoelki@gmx.de>
2020-01-12 12:13:49 +0100

    Add navbar component

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,13 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import Navbar from './components/Navbar.vue';
 
 export default Vue.extend({
   name: 'App',
+
+  components: {
+    Navbar,
+  },
 });
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-navbar>
+    <template v-slot:brandTitle>
+      <h6>Flexbox Visualizer</h6>
+    </template>
+    <v-nav-region position="left">
+      <v-navbar-item>GitHub</v-navbar-item>
+    </v-nav-region>
+    <v-nav-region position="right">
+      <div class="custom-nav-item">
+        <v-toggler id="dark-mode-checkbox" dataCheckIcon="🌤" dataUncheckIcon="🌙"></v-toggler>
+      </div>
+    </v-nav-region>
+  </v-navbar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Navbar',
+});
+</script>
+
+<style lang="scss">
+.header {
+  .custom-nav-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #dark-mode-checkbox {
+    text-align: initial;
+    padding-left: 0;
+  }
+}
+</style>
