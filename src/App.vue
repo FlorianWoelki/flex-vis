5af09b4a70c5867add5f7e63857e5bba76115b72
FlorianWoelki <FlorianWoelki@gmx.de>
2020-01-15 08:59:03 +0100

    Add explanation view

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <div id="app">
     <Navbar />
     <router-view/>
+    <!-- TODO: Remove it and create a all in all view -->
+    <Explanation />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Navbar from './components/Navbar.vue';
+import Explanation from './views/Explanation.vue';
 
 export default Vue.extend({
   name: 'App',
 
   components: {
     Navbar,
+    Explanation,
   },
 });
 </script>
---
+++ src/views/Explanation.vue
@@ -0,0 +1,22 @@
+<template>
+  <section id="explanation-view" class="white u-text-center">
+    <h3>This is a coming up explanation</h3>
+    <v-space large></v-space>
+    <v-btn color="light" size="large" class="font-bold u-center">Get Started!</v-btn>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Explanation',
+});
+</script>
+
+<style lang="scss">
+#explanation-view {
+  padding: 75px;
+  background-color: rgb(94, 92, 199);
+}
+</style>
