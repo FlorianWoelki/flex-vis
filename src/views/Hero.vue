10e1f0ab2e7d0e4d3bcb80c1e81dad334252d088
FlorianWoelki <FlorianWoelki@gmx.de>
2020-01-15 08:52:40 +0100

    Add v-parallax component around the hero section

--- src/views/Hero.vue
+++ src/views/Hero.vue
@@ -1,17 +1,19 @@
 <template>
   <section id="hero-view">
-    <v-container class="u-text-center">
-      <img style="max-height: 250px;" src="@/assets/header-logo.png" alt="header-logo">
-      <h1>Flexbox Visualizer</h1>
-      <h5 class="font-thin">Carefully designed CSS flexbox visualizer for learning purposes.</h5>
-      <v-space large></v-space>
-      <v-btn class="u-center" color="link" size="large">
-        <span class="font-bold">Get Started</span>
-        <i class="fas fa-arrow-right fa-lg"></i>
-      </v-btn>
-      <v-space></v-space>
-      <a class="github-button" href="https://github.com/florianwoelki/flex-vis" data-size="large" data-show-count="true" aria-label="Star florianwoelki/flex-vis on GitHub">Star</a>
-    </v-container>
+    <v-parallax disableParallax fullscreen customBody>
+      <v-container class="u-text-center">
+        <img style="max-height: 250px;" src="@/assets/header-logo.png" alt="header-logo">
+        <h1>Flexbox Visualizer</h1>
+        <h5 class="font-thin">Carefully designed CSS flexbox visualizer for learning purposes.</h5>
+        <v-space large></v-space>
+        <v-btn class="u-center" color="link" size="large">
+          <span class="font-bold">Get Started</span>
+          <i class="fas fa-arrow-right fa-lg"></i>
+        </v-btn>
+        <v-space></v-space>
+        <a class="github-button" href="https://github.com/florianwoelki/flex-vis" data-size="large" data-show-count="true" aria-label="Star florianwoelki/flex-vis on GitHub">Star</a>
+      </v-container>
+    </v-parallax>
   </section>
 </template>
 
