31cf65be9165eeba78275c526a0e964c98012b78
FlorianWoelki <FlorianWoelki@gmx.de>
2020-02-06 11:20:22 +0100

    feat(common): add get started button click action

--- src/components/Explanation.vue
+++ src/components/Explanation.vue
@@ -6,7 +6,9 @@
         It is there to <b>visualize</b> and <b>explain</b> the <b>CSS Flexbox functionality</b>.
       </h5>
       <v-space></v-space>
-      <v-btn color="light" size="large" class="font-bold u-center">Get Started!</v-btn>
+      <v-btn color="light" size="large" class="font-bold u-center" @click="handleGetStarted">
+        Get Started!
+      </v-btn>
     </v-container>
   </section>
 </template>
@@ -16,6 +18,12 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'Explanation',
+
+  methods: {
+    handleGetStarted(): void {
+      this.$router.push({ path: 'aligncontent' });
+    },
+  },
 });
 </script>
 
