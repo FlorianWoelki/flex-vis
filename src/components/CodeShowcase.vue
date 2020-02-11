f2df834afc954272948d09ad257e4b235f21da5b
FlorianWoelki <FlorianWoelki@gmx.de>
2020-02-11 14:52:09 +0100

    style(darkmode): add darkmode for code component

--- src/components/CodeShowcase.vue
+++ src/components/CodeShowcase.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- eslint-disable-->
   <pre>
-    <code class="code-showcase" data-lang="CSS">
+    <code :class="`code-showcase${darkMode ? ' dark' : ''}`" data-lang="CSS">
 <span class="css-class">.container</span> {
   <span class="css-key">display</span>: <span class="css-value">flex</span>;<br v-if="customContent"/>  <slot v-if="customContent"></slot>
   <span class="css-key">{{this.flexAttribute}}</span>: <span class="css-value">{{this.flexOption}}</span>;
@@ -24,6 +24,10 @@ export default class CodeShowcase extends Vue {
 
   @Prop()
   private flexAttribute!: string;
+
+  private get darkMode(): boolean {
+    return this.$store.state.darkMode;
+  }
 }
 </script>
 
