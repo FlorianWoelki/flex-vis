a504f5a57c74012a696bfd8528d875910a887d19
florianwoelki <florianwoelki@gmx.de>
2020-02-18 09:27:52 +0100

    refactor: direction to flex-direction

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -40,7 +40,7 @@ import { Component } from 'vue-property-decorator';
 @Component({})
 export default class Navbar extends Vue {
   private flexOptions: Array<String> = [
-    'order', 'direction', 'grow', 'wrap', 'shrink',
+    'order', 'flex-direction', 'grow', 'wrap', 'shrink',
     'justify-content', 'flex', 'align-self', 'align-items', 'align-content',
   ];
 
