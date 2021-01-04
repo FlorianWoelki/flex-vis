3b5ad4dabb67c673e3dda299961dc93685d52698
florianwoelki <florianwoelki@gmx.de>
2021-01-04 09:12:15 +0100

    fix(comp): reactivity for CodeShowcase

--- src/components/flexOption/CodeShowcase.vue
+++ src/components/flexOption/CodeShowcase.vue
@@ -1,5 +1,11 @@
 <template>
-  <v-code lang="css" :dark="darkMode">{{ code }}</v-code>
+  <!-- eslint-disable-->
+  <pre>
+    <code :class="`code-showcase${darkMode ? ' dark' : ''}`" data-lang="CSS"><span v-if="isFlexItem" class="css-class">.flex-item</span><span v-else class="css-class">.container</span> {
+  <span class="css-key">display</span>: <span class="css-value">flex</span>;<br v-if="customContent"/>  <slot v-if="customContent"></slot>
+  <span class="css-key">{{this.flexAttribute}}</span>: <span class="css-value">{{this.flexOption}}</span>;
+}</code>
+  </pre>
 </template>
 
 <script lang="ts">
@@ -23,12 +29,33 @@ export default class CodeShowcase extends Vue {
   private get darkMode(): boolean {
     return this.$store.state.darkMode;
   }
-
-  private get code(): string {
-    return `${this.isFlexItem ? '.flex-item' : '.container'} {
-  display: flex;
-  ${this.flexAttribute}: ${this.flexOption};${this.$slots.default ? `\n  ${this.$slots.default![0].text}` : ''}
-}`;
-  }
 }
 </script>
+
+<style lang="scss">
+@import '@/scss/_variables.scss';
+
+code {
+  background: #f6f8fa;
+  display: block;
+  color: #333;
+  overflow-x: auto;
+  max-height: 750px;
+
+  &.code-showcase {
+    padding: 1.5rem 1.5rem 1rem !important;
+  }
+
+  .css-key {
+    color: $css-key-color;
+  }
+
+  .css-value {
+    color: $css-value-color;
+  }
+
+  .css-class {
+    color: $css-class-color;
+  }
+}
+</style>
